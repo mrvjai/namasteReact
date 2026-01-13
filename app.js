c8176a095cd96b78f933e3bcb8bc63247e48fba1
vijay <mr.vjai.3@gmail.com>
2026-01-13 23:47:17 +0530

    day2

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
-const test = React.createElement("div",{id:"one"},[React.createElement("h1",null,"hello world!!!!"),React.createElement("h1",null,"hello indiaa!!!!")])
+// ...existing code...
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+// ...existing code...
+
+const test = React.createElement(
+  "div",
+  { id: "one" },
+  [
+    React.createElement("h1", null, "hello world!!!!"),
+    React.createElement("h1", null, "hello indiaa!!!!")
+  ]
+);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(test)
+root.render(test);
+// ...existing code...
