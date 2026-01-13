44a28f461d6abbb54cf02cafbb74251f37fbc686
vijay <mr.vjai.3@gmail.com>
2026-01-13 09:46:18 +0530

    day1

---
+++ app.js
@@ -0,0 +1,3 @@
+const test = React.createElement("div",{id:"one"},[React.createElement("h1",null,"hello world!!!!"),React.createElement("h1",null,"hello indiaa!!!!")])
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(test)
