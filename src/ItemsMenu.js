2585ee5156e175a563b300cc7ec1d19bfacac59d
vijay <mr.vjai.3@gmail.com>
2026-01-31 21:07:26 +0530

    upgrading cart buttons

--- src/ItemsMenu.js
+++ src/ItemsMenu.js
@@ -1,11 +1,17 @@
 import { useDispatch } from "react-redux";
-import { addItem } from "../utils/cartSlice";
+import { addItem, removeItem } from "../utils/cartSlice";
+import {useState} from 'react'
 
 export const ItemsMenu = ({ data }) => {
   const dispatch = useDispatch();
-  const handleAddItem = (x)=>{
-    dispatch(addItem(x))
-  }
+  const [rem,setRem] = useState(0)
+  const handleAddItem = (x) => {
+    dispatch(addItem(x));
+    setRem(1);
+  };
+  const handleRemoveItem = (x) => {
+    dispatch(removeItem(x));
+  };
   return (
     <div>
       {data.map((main, y) =>
@@ -13,9 +19,27 @@ export const ItemsMenu = ({ data }) => {
           <div key={y} className="p-2 m-2 border-gray-200 border-b-2 text-left">
             <div className="py-4 flex justify-between">
               <span>{x.item.name}</span>
-              <button className="order border-black bg-sky-600 p-1" onClick={()=>handleAddItem(x)}>- ADD +</button>
+              <div>
+                <button
+                  className="order border-black bg-lime-200 p-1"
+                  onClick={() => handleAddItem(x)}
+                >
+                  +
+                </button>
+                <button className="order border-black bg-sky-600 p-1">
+                  ADD
+                </button>
+                {rem > 0 ? (
+                  <button
+                    className="order border-black bg-lime-200 p-1"
+                    onClick={() => handleRemoveItem(x)}
+                  >
+                    -
+                  </button>
+                ) : null}
+              </div>
             </div>
-            <p className="text-xs">{x.item.desc||"preparing with love"}</p>
+            <p className="text-xs">{x.item.desc || "preparing with love"}</p>
           </div>
         )),
       )}
